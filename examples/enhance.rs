3c9a8cc5b5e0e2eb05158097835d66d7cc8e3d53
Takeru Ohta <phjgt308@gmail.com>
2022-01-14 19:26:32 +0900

    Update example

--- examples/enhance.rs
+++ examples/enhance.rs
@@ -3,10 +3,13 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Opt {
-    #[structopt(default_value = "examples/test.png")]
     image_path: PathBuf,
-    #[structopt(default_value = "examples/enhanced.png")]
+
+    #[structopt(long, default_value = "enhanced.png")]
     output_path: PathBuf,
+
+    #[structopt(long, default_value = "0.5")]
+    alpha: f32,
 }
 
 fn main() -> anyhow::Result<()> {
@@ -15,38 +18,40 @@ fn main() -> anyhow::Result<()> {
     let file = std::fs::File::open(&opt.image_path)?;
     let decoder = png::Decoder::new(std::io::BufReader::new(file));
     let mut reader = decoder.read_info()?;
-    assert_eq!(reader.info().bit_depth, png::BitDepth::Eight);
 
     let mut buf = vec![0; reader.output_buffer_size()];
     let info = reader.next_frame(&mut buf)?;
+    println!("Image resolution: {}x{}", info.width, info.height);
+    println!("Image bit depth: {:?}", info.bit_depth);
+    println!("Image color type: {:?}", info.color_type);
+    assert_eq!(info.bit_depth, png::BitDepth::Eight);
 
-    if reader.info().color_type == png::ColorType::Rgb {
-    } else if reader.info().color_type == png::ColorType::Rgba {
-        let mut rgb_buf = Vec::new();
-        for i in 0..(info.width * info.height) as usize {
-            rgb_buf.push(buf[i * 4 + 0]);
-            rgb_buf.push(buf[i * 4 + 1]);
-            rgb_buf.push(buf[i * 4 + 2]);
-        }
-        buf = rgb_buf;
-    } else {
-        panic!("Unsupported {:?}", reader.info().color_type);
-    }
-
-    let mut bimef = agcwd::Agcwd::new();
-
+    let agcwd = agcwd::Agcwd::new(opt.alpha);
     let start = std::time::Instant::now();
-    bimef.enhance(&mut buf);
-    eprintln!("Elapsed: {:?}", start.elapsed());
+    match reader.info().color_type {
+        png::ColorType::Rgb => {
+            agcwd.enhance_rgb_image(&mut buf);
+        }
+        png::ColorType::Rgba => {
+            agcwd.enhance_rgba_image(&mut buf);
+        }
+        ty => {
+            panic!("Unsupported color type: {:?}", ty);
+        }
+    }
+    println!("Elapsed: {:?}", start.elapsed());
 
     let mut encoder = png::Encoder::new(
-        std::io::BufWriter::new(std::fs::File::create(opt.output_path)?),
+        std::io::BufWriter::new(std::fs::File::create(&opt.output_path)?),
         info.width,
         info.height,
     );
-    encoder.set_color(png::ColorType::Rgb);
+    encoder.set_color(info.color_type);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header()?;
     writer.write_image_data(&buf)?;
+
+    println!("Output path: {:?}", opt.output_path);
+
     Ok(())
 }
